faa1741fa8455b96fa3e7b4d9c750469868a4bbf
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-05-10 11:14:49 -0500

    Program section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/Programs";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 };
