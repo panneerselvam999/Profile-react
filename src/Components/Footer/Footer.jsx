9b10a0a9158a5f9cb5d4956c5f7b037a1d61404a
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-05-10 15:22:58 -0500

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p> 2024 Edusity. All rights reserves.</p>
+            <ul>
+                <li>Terms of Sevices</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </footer>
+
+    )
+}
+
+export default Footer
